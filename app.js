ad956475573ee17034e287bc093cd97615a19355
BrendaAbigailVC <109320578+BrendaAbigailVC@users.noreply.github.com>
2025-07-04 10:15:37 -0500

    Obtener preguntas del cuestionario

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const authenticateJWT = require('./config/middleware'); // Importar el middlewar
 const authRoutes = require('./app/routes/authRoutes');
 const courseRoutes = require('./app/routes/courseRoutes');
 const reviewRoutes = require('./app/routes/reviewRoutes');
+const rateRoutes = require('./app/routes/rateRoutes');
 const collegeRoutes = require('./app/routes/collegeRoutes')
 const teacherRoutes = require('./app/routes/teacherRouter')
 const userRoutes = require('./app/routes/userRoutes')
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use('/api/users', authRoutes);
 app.use('/api/courses', authenticateJWT, courseRoutes);  // Rutas de cursos protegidas
 app.use('/api/reviews', authenticateJWT, reviewRoutes);  // Rutas de valoraciones protegidas
+app.use('/api/rate', authenticateJWT, rateRoutes);  // Rutas de evaluacion protegidas
 app.use('/api/colleges', authenticateJWT, collegeRoutes)
 app.use('/api/teachers', authenticateJWT, teacherRoutes)
 app.use('/api/users', authenticateJWT, userRoutes)
--- app/routes/rateRoutes.js
+++ app/routes/rateRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const evaluationController = require('../controllers/evaluationController'); 
+const evaluationController = require('../controllers/rateController'); 
 
 // Ruta para obtener las preguntas y sus opciones
 router.get('/', evaluationController.getGroupsWithLabels);
